e072d8dc9e525083ea47c26e8b898b263b0c4ae7
Alfonso Murrieta <alfonsomvmx@outlook.com>
2021-04-17 01:15:26 -0500

    removido por solicitud

--- EDA2/laboratorio/practicas/10/tistaCodes/main/src/main/Murrieta_Valdespino_Practica10.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Murrieta_Valdespino_Practica10 {
-    public static void main(String[] args) {    
-        ventana ventana1 = new ventana();
-        ventana1.setVisible(true);
-        ventana1.setLocationRelativeTo(null);
-    }
-}
--- EDA2/laboratorio/practicas/11/Codes/0prueba.c
+++
@@ -1,8 +0,0 @@
-#include <omp.h>
-#include <stdio.h>
-
-int main(){
-#pragma omp parallel
-printf("\n Prueba hilo (thread) %d de %d  ", omp_get_num_threads(), omp_get_num_threads() );
-return 0;
-}
